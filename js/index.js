ded6714349679e0b9e49f819238321334def47b1
Luiz Paulo Gonçalves <37484860+LuizPauloLPG2@users.noreply.github.com>
2019-03-30 17:25:35 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,39 @@
+angular.module("application", []);
+angular.module("application").controller("applicationController", function ($scope){
+  $scope.message = 'application initialize';
+  $scope.title = 'People';
+  // $scope.people = [
+  //   {name: 'Luiz', email: 'luizgroup@gmail.com', color: 'blue'},
+  //   {name: 'steve', email: 'stevegroup@gmail.com', color: 'purple'},
+  // ];
+  $scope.people = [
+    {name: 'Luiz', email: 'luizgroup@gmail.com', age: 19},
+    {name: 'steve', email: 'stevegroup@gmail.com', age: 27},
+  ];
+
+  $scope.languages = [
+    {name: 'PHP', frameword: 'Zend', type: 'BACK-END'},
+    {name: 'HTML', frameword: 'Bootstrap', type: 'FRONT-END'},
+  ];
+
+  $scope.addPerson = function(person){
+    // $scope.people.push(angular.copy(person));
+    $scope.people.push(person);
+    delete $scope.person;
+  }
+
+  $scope.delPerson = function(people){
+    $scope.people = people.filter(function (person){
+      if(!person.selected){
+        return person;
+      }      
+    });
+  }
+
+  $scope.isPersonSelected = function(people){
+    return people.some(function (person){
+      return person.selected;
+    });
+  }
+
+});
